afc67f645c6c5931b8e482ce73506a7ebfdd2124
MrSayyod <ssayyod@gmail.com>
2022-07-15 10:42:05 -0600

    Set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  root "news#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :articles
+    end
+  end
+
+  get '*path', to: 'news#index', via: :all
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
